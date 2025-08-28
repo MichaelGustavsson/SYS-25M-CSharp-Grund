89a8b8af57b0fa6a2386f83945b7d64b1b05a655
MichaelGustavsson <michael.gustavsson@skaftoskorpan.se>
2025-08-28 15:03:29 +0200

    Solution for lesson 2

---
+++ Lektion-02/Intro/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Intro;
+
+class Program
+{
+    static void Main()
+    {
+        // Deklarerar explicit en variabel av typen string...
+        string name;
+
+        // Läser in ifrån terminalen...
+        name = Console.ReadLine();
+
+        // Skriver ut till terminalen...
+        Console.WriteLine(name);
+
+    }
+}
+
+
+// option + 4 = $
+// shift + option + 8 = {
+// shift + option + 9 = }
+// option + 8 = [
+// option + 9 = ]
+// shift + option + 7 = \
+// option + 7 = |
+
+// var name = "Michael";
+// var age = 25;
+// var amount = 0;
+// var isOk = true;
+// var balance = 12.5;
+// var vehicle = new { mileage= 10};
+
+// string name = "Michael";
+// name = 25;
+
+
+// Implicit declaration...
+// var name = "???";
+// Explicit declaration...
+// string name;
+
+// name = "Michael";
+
+// $"Hej på dig,{name}"
+// Template string med en dynamisk plats({name})
+// Skapar en interpollation => skapa en ny sträng
+// med Hej på dig, och sedan hämtar värdet på name
+// och skriver ut Michael.
+// Console.WriteLine($"Hej på dig, {name}");
---
+++ Lektion-02/atm/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace atm;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Deklarerar en variabel för att hantera saldo...
+        int balance;
+        // Insättning...
+        Console.WriteLine("Ange hur mycket du vill sätta in");
+        string? amount = Console.ReadLine();
+
+        // if (amount is null || amount.Length == 0)
+        // if (amount is null || amount.Trim().Length == 0)
+        if (string.IsNullOrWhiteSpace(amount))
+        {
+            Console.WriteLine("Du MÅSTE ange hur mycket du vill sätta in!!!");
+            // Tvinga applikationen att DÖ!
+            Environment.Exit(0);
+        }
+        // Konvertera amount ifrån string => int...
+        // Cast, Parse, Convert
+        balance = int.Parse(amount);
+        // Skriv ut balansen(saldot)...
+        DisplayBalance(balance);
+
+        // Skriv ett kommando som frågar efter hur mycket som ni vill ta
+        Console.WriteLine("Hur mycket vill du ta ut?");
+        // Hämta in ifrån terminalen värdet som ni vill ta ut
+        amount = Console.ReadLine();
+        // Skriv ut hur mycket ni vill ta ut...
+        Console.WriteLine("Du har tagit ut: " + amount);
+
+        // Räkna balansen/Saldot och skriva saldot.
+        balance -= int.Parse(amount);
+        // balance = balance - int.Parse(amount);
+
+        DisplayBalance(balance);
+    }
+
+    // Egen metod för att visa saldot...
+    static void DisplayBalance(int balance) // Metodens spefication/header/interface
+    {
+        Console.WriteLine("Ditt saldo är: " + balance);
+    }
+}
