aa6d854cd369c9f85ec42ae0529c5d05fdabacfa
MichaelGustavsson <michael.gustavsson@skaftoskorpan.se>
2025-09-01 13:24:55 +0200

    Added basic and strings

---
+++ Lektion-03/basic/Program.cs
@@ -0,0 +1,76 @@
+﻿namespace basic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // ===========================================================
+        // Explicit typing...
+        // ===========================================================
+
+        // Skapar en plats i minnet för att lagra data som får namnet balance...
+        // Skapar en låda med namnet balance
+        // Deklarera en variabel...
+        // och initierar variabeln med värdet 10...
+        // ByValue...
+        int balance = 10;
+        Console.WriteLine($"Balance har nu värdet: {balance}");
+
+        // ===========================================================
+        // Heltal(int, long) inga decimaler
+        // ===========================================================
+        // värdet på balance variabeln(10) kopieras till saldo
+        // Copy ByValue...
+        int saldo = balance;
+        Console.WriteLine($"Saldo har nu värdet: {saldo}");
+
+        balance = 20;
+        Console.WriteLine($"Balance har nu värdet: {balance}");
+
+        saldo = 30;
+        Console.WriteLine($"Saldo har nu värdet: {saldo}");
+        Console.WriteLine($"Balance har nu värdet: {balance}");
+
+        // ===========================================================
+        // Strängar(string)
+        // ===========================================================
+        // Variabler använder sig av camelCase namngivning...
+        string firstName = "Michael";
+        Console.WriteLine($"Ditt namn är {firstName}");
+
+        string nickName = firstName;
+        Console.WriteLine($"Ditt smeknamn är {nickName}");
+
+        nickName = "Micke";
+        Console.WriteLine($"Ditt namn är {firstName}");
+        Console.WriteLine($"Ditt smeknamn är {nickName}");
+
+        // ===========================================================
+        // Flyttal(double, decimal) decimaler
+        // ===========================================================
+        double amount = 25.50;
+        Console.WriteLine($"Värdet är nu: {amount}");
+
+        decimal size = 12.5M;
+        Console.WriteLine($"Storleken är nu: {size}");
+
+        // ===========================================================
+        // Booleans(true, false)
+        // ===========================================================
+        bool isValid = true;
+        Console.WriteLine($"Är det ok? {isValid}");
+
+        // PascalCase används i C# för klasser och metoder...
+        // DisplayBalance(){}
+
+        // ===========================================================
+        // Implicit typing...
+        // ===========================================================
+        var lastName = "100";
+        Console.WriteLine($"Ditt efternamn är: {lastName}");
+
+        lastName = "Gustavsson";
+        Console.WriteLine($"Ditt efternamn är: {lastName}");
+
+    }
+}
---
+++ Lektion-03/stringDemo/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Runtime.InteropServices;
+
+namespace stringDemo;
+
+class Program
+{
+    static void Main()
+    {
+        string name = "Jensen Education";
+        Console.WriteLine(name);
+
+        // Console.WriteLine(name.Count());
+        Console.WriteLine(name.Length);
+
+        // string partOfName = name.Substring(0);
+        string partOfName = name[..];
+        Console.WriteLine(partOfName);
+
+        // partOfName = name.Substring(1);
+        partOfName = name[1..];
+        Console.WriteLine(partOfName);
+
+        // partOfName = name.Substring(0, 6);
+        partOfName = name[..6];
+        Console.WriteLine(partOfName);
+
+        // partOfName = name.Substring(7);
+        partOfName = name[7..];
+        Console.WriteLine(partOfName);
+
+        double value = 7988.67950;
+        Console.WriteLine("Värdet är: {0}", value);
+        Console.WriteLine("Värdet är nu: {0:C2}", value);
+        Console.WriteLine("Värdet är nu: {0:N2}", value);
+        // Console.WriteLine("Värdet är nu: {0:F2}", value);
+        // Console.WriteLine("Värdet är nu: {0:G2}", value);
+        // C = Currency...
+        // N = Fasta decimal med tusen separator...
+        // D = Decimal
+        // F = Fast decimaler
+        // E = Exponenter
+        // G = Generell formatering(standard)
+
+        string firstName = "Michael";
+        string lastName = "Gustavsson";
+        // string fullName = firstName + " " + lastName;
+        string fullName = string.Concat(firstName, " ", lastName);
+
+        Console.WriteLine("Ditt fullständiga namn är: {0}", fullName);
+
+        Console.WriteLine("Delar av ditt namn finns: {0}", fullName.Contains("Michael"));
+        Console.WriteLine("Delar av ditt namn finns: {0}", fullName.Contains("ich"));
+        Console.WriteLine("Delar av ditt namn finns: {0}", fullName.Contains("son"));
+        Console.WriteLine("Delar av ditt namn finns: {0}", fullName.Contains("ols"));
+
+        string[] words = name.Split(" ");
+
+        Console.WriteLine("Första ordet är {0}", words[0]);
+        Console.WriteLine("Andra ordet är {0}", words[1]);
+
+    }
+}
