00ad3b325f246ebc00c6a5d7327a4b7c291d9ab7
MichaelGustavsson <michael.gustavsson@skaftoskorpan.se>
2025-09-01 15:25:35 +0200

    Added rest of day 3

---
+++ Lektion-03/SimpleClassesAndStructures/Program.cs
@@ -0,0 +1,85 @@
+﻿namespace SimpleClassesAndStructures;
+
+class Program
+{
+    static void Main()
+    {
+        // ==================================================
+        // Skapa egna datatyper/datastrukturer
+        // ==================================================
+
+        // Struct
+        // Skapar en instans(kopia)
+        var position = new Point();
+        position.x = 450;
+        position.y = 600;
+
+        Console.WriteLine("Positionens värde: {0} : {1}", position.x, position.y);
+
+        position.Draw();
+
+        // Class
+        // new Vehicle skapar ett objekt med namnet volvo
+        var volvo = new Vehicle();
+        volvo.make = "Volvo";
+        volvo.model = "XC60";
+        volvo.mileage = 50000;
+        volvo.modelYear = 2018;
+
+        var ford = new Vehicle();
+        ford.make = "Ford";
+        ford.model = "MACH-E";
+        ford.mileage = 20000;
+        ford.modelYear = 2023;
+
+        Console.WriteLine("Bilens information: {0}", volvo.make);
+        Console.WriteLine("Bilens information: {0}", volvo.model);
+        Console.WriteLine("Bilens information: {0}", volvo.mileage);
+        Console.WriteLine("Bilens information: {0}", volvo.modelYear);
+
+        Console.WriteLine("Bilens information: {0}", ford.make);
+        Console.WriteLine("Bilens information: {0}", ford.model);
+        Console.WriteLine("Bilens information: {0}", ford.mileage);
+        Console.WriteLine("Bilens information: {0}", ford.modelYear);
+
+        Vehicle[] vehicles = [
+            new Vehicle{make="Fiat",model="Uno",mileage=250000,modelYear=2011},
+            new Vehicle{make="Kia",model="Ceed",mileage=125000,modelYear=2015},
+            new Vehicle{make="Mercedes",model="S500",mileage=450000,modelYear=2008}
+        ];
+
+        var vehicle = vehicles.Last();
+        Console.WriteLine("Bilen är en {0} {1}", vehicle.make, vehicle.model);
+    }
+
+    // Class
+    public class Vehicle
+    {
+        public string make = "";
+        public string model = "";
+        public int mileage;
+        public int modelYear;
+    }
+
+
+    // Struct
+    public struct Point
+    {
+        // Databärande variabler/data
+        public int x = 0;
+        public int y = 0;
+
+        public Point()
+        {
+        }
+
+        // Metoder...
+        // Metoder utför något baserat på data som finns i structen...
+        public void Draw()
+        {
+            Console.WriteLine("Jag kommer att rita en linje mellan punkterna: x={0} y={1}",
+                x, y);
+        }
+
+    }
+}
---
+++ Lektion-03/simpleArrays/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace simpleArrays;
+
+class Program
+{
+    static void Main()
+    {
+        // Array en datastruktur som lagrar information/data
+        // linjärt. Data som lagras kallas för element...
+        // Vi kan skapa arrayer ifrån vilken datatyp som helst...
+        // Åtkomst till elementen(datat) sker via elements index(position)
+
+        // ============================================================
+        // Låt oss skapa en array som lagrar element av datatypen int
+        // ============================================================
+        // Metod 1.        
+        int[] numbers = new int[5];
+
+        // Lägg till data i en arrays element...
+        numbers[0] = -1;
+        numbers[1] = 1;
+        numbers[2] = 2;
+        numbers[3] = 3;
+        numbers[4] = 4;
+        // numbers[5] = 5;
+
+        Console.WriteLine("Värdet i byrån i låda 1: {0}", numbers[0]);
+        Console.WriteLine("Hur många lådor finns det i byrån? {0}", numbers.Length);
+
+        // Metod 2.
+        int[] values = [-1, 2, 3, 4, 5, 6, 7, 8, 9];
+        Console.WriteLine("Value på position 2 i array values: {0}", values[1]);
+
+        // ============================================================
+        // Låt oss skapa en array som lagrar element av datatypen string
+        // ============================================================
+        string[] names = ["Wilma", "Simon", "Jocke", "My"];
+
+        Console.WriteLine("Vad heter du på position 2 i listan? {0}", names[1]);
+        names[1] = "Filip";
+        Console.WriteLine("Vad heter du på position 2 i listan? {0}", names[1]);
+
+        // Går icke!!!
+        // names[5] = "Hassan";
+
+        // ============================================================
+        // Låt oss skapa en array som lagrar element av typen
+        // anonyma objekt...
+        // ============================================================
+
+        object[] vehicles = [
+            new {make="Volvo", model="XC60", mileage=50000, modelYear=2018},
+            new {make="Ford", model="MACH-E", mileage=20000, modelYear=2022},
+            new {make="KIA", model="EV6", mileage=45000, modelYear=2023},
+        ];
+
+        Console.WriteLine(vehicles[2]);
+        var vehicle = vehicles[vehicles.Length - 1];
+        Console.WriteLine(vehicle);
+        var lastVehicle = vehicles.Last();
+        Console.WriteLine(lastVehicle);
+        var firstVehicle = vehicles.First();
+        Console.WriteLine(firstVehicle);
+    }
+}
