cb96786c4575ffed57f92a1f184482d1396dcfb1
MichaelGustavsson <michael.gustavsson@skaftoskorpan.se>
2025-09-03 15:41:25 +0200

    Finalized application ATM and added a demo for ReadKey()

--- Lektion-04/ATM/Program.cs
+++ Lektion-04/ATM/Program.cs
@@ -5,33 +5,60 @@ class Program
 {
     static void Main()
     {
+        Console.WriteLine("-----------------------------------------------------------------------");
+        Console.WriteLine("För att avsluta programmet tryck på tangent 'x' och sedan 'Enter'");
+        Console.WriteLine("För att se saldo tryck på tangent 'b'");
+        Console.WriteLine("För att sätta in pengar tryck på tangenten 'd'");
+        Console.WriteLine("För att ta ut pengar tryck på tangenten 'w'");
+        Console.WriteLine("För att se senaste transaktionerna tryck på tangenten 't'");
+        Console.WriteLine("-----------------------------------------------------------------------");
+
         // BankAccount account = new BankAccount();
         // var account = new BankAccount();
         BankAccount account = new();
 
-        // Visa saldot...
-        Console.WriteLine("Aktuellt saldo: {0}", account.GetBalance());
-
-        // Sätt in pengar...
-        account.Deposit(500);
-
-        // Visa saldot...
-        Console.WriteLine("Aktuellt saldo: {0}", account.GetBalance());
-
-        // Ta ut lite pengar...
-        try
+        while (true)
         {
-            account.Withdraw(25);
-            account.Withdraw(1000);
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine("Det gick fel, meddelande: {0}", ex.Message);
-        }
-        finally
-        {
-            // Visa saldot...
-            Console.WriteLine("Aktuellt saldo: {0}", account.GetBalance());
+            string? key = Console.ReadLine();
+
+            if (key == "x")
+            {
+                Environment.Exit(0);
+            }
+            else if (key == "b")
+            {
+                var balance = account.GetBalance();
+                Console.WriteLine("Aktuellt saldo: {0}", balance);
+            }
+            else if (key == "d")
+            {
+                Console.WriteLine("Hur mycket vill du sätta in?");
+                int amount = int.Parse(Console.ReadLine()!);
+                account.Deposit(amount);
+            }
+            else if (key == "w")
+            {
+                Console.WriteLine("Hur mycket vill du ta ut?");
+                // int amount = int.Parse(Console.ReadLine()!);
+
+                if (int.TryParse(Console.ReadLine(), out int amount))
+                {
+                    account.Withdraw(amount);
+                }
+            }
+            else if (key == "t")
+            {
+                var transactions = account.GetTransactions();
+
+                foreach (var tran in transactions)
+                {
+                    Console.WriteLine("Transaktionens värde: {0}", tran);
+                }
+            }
+            else
+            {
+                Environment.Exit(0);
+            }
         }
     }
 }
@@ -41,11 +68,18 @@ public class BankAccount
     // Vi behöver en variabel för att spara undan tillgängligt saldo...
     int balance = 0;
 
+    // Lagra insättningar och uttag => transaktioner...
+    // Vi skapar en dynamisk array som lagrar heltal...
+    List<int> transactions = [];
+    // List<int> transaction = new List<int>();
+
     public void Deposit(int value)
     {
         // Öka värdet på saldot...
         // balance = balance + value;
         balance += value;
+        // Lägg till transaktion till vår transaktionslista...
+        transactions.Add(value);
     }
 
     public void Withdraw(int value)
@@ -59,11 +93,19 @@ public class BankAccount
         else
         {
             balance -= value;
+
+            // Lägg till transaktion till vår transaktionslista...
+            transactions.Add(value);
         }
     }
 
     public int GetBalance()
     {
         return balance;
+    }
+
+    public List<int> GetTransactions()
+    {
+        return transactions;
     }
 }
---
+++ Lektion-04/ReadKeyDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ReadKeyDemo;
+
+class Program
+{
+    static void Main()
+    {
+        ConsoleKeyInfo keyInfo;
+
+        while (true)
+        {
+            keyInfo = Console.ReadKey(true);
+
+            if (keyInfo.Key == ConsoleKey.Escape)
+            {
+                Environment.Exit(0);
+            }
+            else if (char.IsNumber(keyInfo.KeyChar) || keyInfo.Key == ConsoleKey.Enter)
+            {
+                while (char.IsNumber(keyInfo.KeyChar))
+                {
+                    Console.WriteLine("Du gjorde följande inmatning: {0}", keyInfo.KeyChar);
+                    keyInfo = Console.ReadKey(true);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Du måste ange ett numeriskt värde!");
+            }
+        }
+    }
+}
