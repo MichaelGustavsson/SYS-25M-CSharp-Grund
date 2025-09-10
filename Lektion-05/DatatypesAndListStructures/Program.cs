faf6f8344091a3fda1d73ef3babbe54c59bc2ad3
MichaelGustavsson <michael.gustavsson@skaftoskorpan.se>
2025-09-10 15:51:15 +0200

    Added code lesson 5

---
+++ Lektion-05/DatatypesAndListStructures/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace DatatypesAndListStructures;
+
+class Program
+{
+    static void Main()
+    {
+        // DataTypes .NET BaseType...
+        Console.WriteLine(".NET Base Type......");
+        object obj = new();
+
+        Console.WriteLine("HashCode: {0}", obj.GetHashCode());
+        Console.WriteLine("Type: {0}", obj.GetType());
+
+        Console.WriteLine("");
+        Console.WriteLine(".NET Integer(int)......");
+        int x = 10;
+
+        Console.WriteLine("HashCode: {0}", x.GetHashCode());
+        Console.WriteLine("Type: {0}", x.GetType());
+        Console.WriteLine("CompareTo: {0}", x.CompareTo(20));
+        Console.WriteLine("ToString(): {0}", x.ToString());
+
+        Console.WriteLine("");
+        Console.WriteLine(".NET String(string)......");
+        string name = "Nisse Nilsson";
+
+        Console.WriteLine("HashCode: {0}", name.GetHashCode());
+        Console.WriteLine("Type: {0}", name.GetType());
+        Console.WriteLine("Length of string: {0}", name.Length);
+        Console.WriteLine("OldString: {0} newString: {1}",
+            name, name.Replace("Nisse Nilsson", "Nils Nilsson")
+        );
+        Console.WriteLine("Concat: {0}",
+            string.Concat("Fan", " ", "vad C# är kul🤩"));
+
+
+        Console.WriteLine("");
+        Console.WriteLine(".NET DateTime................");
+
+        // DateTime testDate = DateTime.Today;
+        DateTime testDate = DateTime.Now;
+        Console.WriteLine("Aktuell datum och tid: {0}", testDate);
+        Console.WriteLine("Aktuell datum och tid: {0}", testDate.Date);
+        Console.WriteLine("Aktuell datum och tid: {0}", testDate.TimeOfDay);
+        Console.WriteLine("Aktuell datum och tid: {0}", testDate.Year);
+        Console.WriteLine("Aktuell datum och tid: {0}", testDate.Month);
+        Console.WriteLine("Aktuell datum och tid: {0}", testDate.DayOfWeek);
+        Console.WriteLine("Aktuell datum och tid: {0}", testDate.DayOfYear);
+        Console.WriteLine("Aktuell datum och tid: {0}", testDate.IsDaylightSavingTime());
+
+        Console.WriteLine("Aktuell datum och tid: {0}", testDate.AddMonths(2));
+        Console.WriteLine("Aktuell datum och tid: {0}", testDate.AddMonths(-2));
+        Console.WriteLine("Aktuell datum och tid: {0}", testDate.AddDays(2));
+        Console.WriteLine("Aktuell datum och tid: {0}", testDate.AddDays(-2));
+
+    }
+}
---
+++ Repetitioner/Lektion-04/decisions-and-loops/Program.cs
@@ -0,0 +1,131 @@
+﻿namespace decisions_and_loops;
+
+class Program
+{
+    // Applikationens EntryPoint...
+    static void Main()
+    {
+        // ========================================================
+        // if och else, else if
+        // ========================================================
+        int x = 3;
+
+        if (x < 5)
+        {
+            // Om x är mindre än 5 så hamnar vi här
+            Console.WriteLine("x = {0},alltså mindre än 5", x);
+            Console.WriteLine("x = {0}, också x = {1},alltså mindre än 5", x, x);
+            Console.WriteLine($"x = {x},alltså mindre än 5");
+            if (x == 2)
+            {
+
+            }
+        }
+        else if (x == 4)
+        {
+
+        }
+        else
+        {
+            // x är inte mindre än 5
+        }
+
+        var ok = x < 5 ? "Mindre än 5" : "Inte mindre än 5";
+
+        // ===========================================================
+        // Switch case alternativ till if else if else if...
+        // ===========================================================
+        switch (x)
+        {
+            case 1:
+                // Gör något
+                break;
+            case 2:
+                // Gör något annat...
+                break;
+            default:
+                // Fanns ingen träff
+                break;
+        }
+
+        // ===========================================================
+        // Iterationer = loopar...
+        // ===========================================================
+        // Den klassiska varianten...
+        for (int y = 0; y < 10; y++) { }
+
+        string[] vehicles = ["Volvo", "Fiat", "Ford", "Kia", "Volkswagen"];
+        // Mer moderna varianten...
+        foreach (var vehicle in vehicles)
+        {
+            // Gör något med varje bil...
+        }
+
+        List<string> movies = new List<string>
+        {
+            "Fantomen",
+            "Batman",
+            "Bucket list"
+        };
+
+        movies.Add("Svampbob");
+
+        // Deklaration = skapa en lista som heter movies som
+        // lagrar strängar...
+        // List<string> movies = new List<string>();
+        // movies.Add("Fantomen");
+        // movies.Add("Batman");
+        // movies.Add("Bucket list");
+
+        // List<string> movies = ["Fantomen", "Batman", "Bucket list"];
+
+        // movies.ForEach(() => { });
+
+        // Klassikerna ända tillbaka till windows 3.11(C)
+        // while loopen
+        int i = 0;
+        while (i < 5)
+        {
+            // Gör något...
+            i++;
+        }
+
+        // do while loopen...
+        do
+        {
+            // Garanterade att alltid köra minst en gång.
+            i++;
+        } while (i < 5);
+
+        // Defensiv kodning...
+        // if är en del av defensiv programmering...
+        // try catch finally...
+        try
+        {
+            // försök att göra detta...
+        }
+        catch (NullReferenceException ex)
+        {
+
+        }
+        catch (ArgumentNullException ex)
+        {
+
+        }
+        catch (Exception ex)
+        {
+
+        }
+        // catch
+        // {
+        //     // Här hamnar vi om det inte gick att göra...
+        // }
+        finally
+        {
+            // Här hamnar vi alltid oavsett vad som händer ovan...
+            // Städa upp efter oss...
+        }
+    }
+
+
+}
