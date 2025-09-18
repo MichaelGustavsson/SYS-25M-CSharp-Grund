b97fda733b8a77065ad24510ad80d750c6ef4759
MichaelGustavsson <michael.gustavsson@softtech-dev.se>
2025-09-18 08:16:25 +0200

    Added starter project for lesson 8

---
+++ Lektion-08/WestcoastBank-Starter/Bank.App/Program.cs
@@ -0,0 +1,29 @@
+﻿using Bank.Domain.Models;
+
+namespace Bank.App;
+
+class Program
+{
+    static void Main()
+    {
+        // Använder objekt initiering...
+        BankAccount account = new("Stig Karlsson", 500);
+
+        Console.WriteLine("Konto skapat: {0} AccountNumber: {1}, Kontohavare: {2} - Saldo: {3}",
+            account.CreatedDate, account.AccountNumber, "???", account.Balance);
+
+        Console.WriteLine("Konto skapat: {0} AccountNumber: {1}, Kontohavare: {2} - Saldo: {3}",
+            account.CreatedDate, account.AccountNumber, "???", account.Balance);
+
+        // BankAccount basicAccount = new("Stig Karlsson") { Balance = 75 };
+        // Console.WriteLine("Kontonummer: {0}", basicAccount.AccountNumber);
+        // BankAccount basicAccount = new BankAccount("Stig Karlsson") { _balance = 75 };
+        // BankAccount basicAccount = new BankAccount{_balance = 10};
+        // Denna rad anropar Constructor metoden i BankAccount klassen...
+        // BankAccount basicAccount = new BankAccount("Stig Karlsson", 500);
+        // BankAccount basicAccount = new BankAccount("Stig Karlsson");
+        // BankAccount basicAccount = new BankAccount();
+
+        // Console.WriteLine(basicAccount.GetAccountInfo());
+    }
+}
